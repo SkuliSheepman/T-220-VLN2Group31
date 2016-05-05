98f83eb0b4a343034a261cb19ab8aa7e85f42cd7
Hilmar Tryggvason <hilmart@gmail.com>
2016-05-05 14:45:02 +0000

    Renamed admin view. Added some admin stuff

--- Codex/Codex/Controllers/AdminController.cs
+++ Codex/Codex/Controllers/AdminController.cs
@@ -9,11 +9,25 @@ namespace Codex.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
-        public ActionResult Index()
+        public ActionResult Users()
         {
             UserViewModel model = new UserViewModel();
             return View(model);
+        }
+
+        public ActionResult Courses()
+        {
+            CourseViewModel model = new CourseViewModel();
+            return View(model);
+        }
+
+        public ActionResult CreateUser(NewUserViewModel newUser) {
+            var testJson = new { cakeName = newUser.Name, cakeEmail = newUser.Email };
+            return Json(testJson);
+        }
+
+        public ActionResult CreateCourse(NewCourseViewModel NewCourse) {
+            return null;
         }
     }
 }
--- Codex/Codex/Models/AdminViewModels.cs
+++ Codex/Codex/Models/AdminViewModels.cs
@@ -8,6 +8,28 @@ namespace Codex.Models
 {
     public class UserViewModel
     {
-        public List<UserHelperModel> Users;
+        public NewUserViewModel NewUserModel { get; set; }
+        public List<UserHelperModel> Users { get; set; }
+    }
+
+    public class NewUserViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public List<UserCoursesHelperModel> UserCourses { get; set; }
+    }
+
+    public class CourseViewModel
+    {
+        public NewCourseViewModel NewCourseModel { get; set; }
+        public List<CourseHelperModel> Courses { get; set; }
+    }
+
+    public class NewCourseViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Semester { get; set; }
+        public int Year { get; set; }
     }
 }
