05d1e7581a59509775165e5b340384d22c5a3b43
jonh <jonhs15@ru.is>
2016-05-05 15:17:30 +0000

    problem service

---
+++ Codex/Codex/Services/ProblemService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Codex.DAL;
+using Codex.Models;
+
+namespace Codex.Services
+{
+    public class ProblemService
+    {
+        private Database _db;
+
+        public ProblemService()
+        {
+            _db = new Database();
+        }
+
+        public List<ProblemViewModel> GetAllProblemInCourse(int courseId)
+        {
+            var problem = 
+
+            return 
+        }
+    }
+}
