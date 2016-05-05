4b2596b023835c720c722673b93ef29671520636
SkuliSheepman <skulisheepman94@gmail.com>
2016-05-05 14:24:36 +0000

    Added some dank ass viewmodels and services

---
+++ Codex/Codex/Models/AssignmentViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Codex.Models
+{
+
+    public class AssignmentViewModel
+    {
+
+        public int Id { get; set; }
+        public int CourseInstanceId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<ProblemViewModel> AssignmentProblems { get; set; }
+
+    }
+
+}
