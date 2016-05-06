949cecab7acbd8f4497c6efa6488dea6bc8e7ed9
jonh <jonhs15@ru.is>
2016-05-06 08:14:21 +0000

    problem service fall

--- Codex/Codex/Services/ProblemService.cs
+++ Codex/Codex/Services/ProblemService.cs
@@ -16,5 +16,24 @@ namespace Codex.Services
         {
             _db = new Database();
         }
+        public List<ProblemViewModel> GetAllProblemsInAssignment(int Id)
+        {
+            var problems = (from _assignment in _db.Assignments
+                            join _relation in _db.AssignmentProblems on _assignment.Id equals _relation.AssignmentId
+                            join _problem in _db.Problems on _relation.ProblemId equals _problem.Id
+                            select _problem).Select(_problem => new ProblemViewModel
+                            {
+
+                                Id = _problem.Id,
+                                Name = _problem.Name,
+                                Description = _problem.Description,
+                                Filetype = _problem.Filetype,
+                                Attachment = _problem.Attachment,
+                                Language = _problem.Language
+
+                            }).ToList();
+            return problems;
+        }
+
     }
 }
