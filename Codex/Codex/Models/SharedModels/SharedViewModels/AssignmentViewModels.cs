21f4213e278c6037cb0fdf3c33c6080cbe3b8307
SkuliSheepman <skulisheepman94@gmail.com>
2016-05-06 12:41:39 +0000

    jon do dis

--- Codex/Codex/Models/SharedModels/SharedViewModels/AssignmentViewModels.cs
+++ Codex/Codex/Models/SharedModels/SharedViewModels/AssignmentViewModels.cs
@@ -27,7 +27,7 @@ namespace Codex.Models.SharedModels.SharedViewModels
         public int CourseInstanceId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public DateTime? StartTIme { get; set; }
+        public DateTime? StartTime { get; set; }
         public DateTime? EndTIme { get; set; }
         public int MaxCollaborators { get; set; }
         public List<int> AssignmentProblems { get; set; }
