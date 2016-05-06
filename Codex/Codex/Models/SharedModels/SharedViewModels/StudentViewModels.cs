32f8beee256371409047ab8a46b1321e204f6bcc
Aron Þráinn <aronthrainn@gmail.com>
2016-05-06 12:58:29 +0000

    Added student contr, view and model

---
+++ Codex/Codex/Models/SharedModels/SharedViewModels/StudentViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Codex.Models.SharedModels.SharedViewModels
+{
+    public class StudentViewModel
+    {
+        //public UserViewModel User { get; set; }
+        public List<AssignmentViewModel> Assignments { get; set; }
+    }
+
+}
