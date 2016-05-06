e5ef9c91bf40c9f4d46c1c085ed8cc56ed658361
jonh <jonhs15@ru.is>
2016-05-06 08:45:42 +0000

    problem service

--- Codex/Codex/Services/ProblemService.cs
+++ Codex/Codex/Services/ProblemService.cs
@@ -34,6 +34,33 @@ namespace Codex.Services
                             }).ToList();
             return problems;
         }
+        public void DeleteProblem(int Id)
+        {
+            var problem = _db.Problems.Where(x => x.Id == Id).FirstOrDefault();
+
+            if (problem != null)
+            {
+                _db.Problems.Remove(problem);
+            }
+            _db.SaveChanges();
+        }
+        public List<ProblemViewModel> GetAllProblemsInCourse(int Id)
+        {
+            var problems = (from _course in _db.Courses
+                            join _problem in _db.Problems on _course.Id equals _problem.CourseId
+                            select _problem).Select(_problem => new ProblemViewModel
+                            {
+
+                                Id = _problem.Id,
+                                Name = _problem.Name,
+                                Description = _problem.Description,
+                                Filetype = _problem.Filetype,
+                                Attachment = _problem.Attachment,
+                                Language = _problem.Language
+
+                            }).ToList();
+            return problems;
+        }
 
     }
 }
