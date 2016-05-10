b64fb52bb25a061ef694e088a979e56f9534f4ef
SkuliSheepman <skulisheepman94@gmail.com>
2016-05-10 10:36:15 +0000

    wwatip+

--- Codex/Codex/Controllers/TeacherController.cs
+++ Codex/Codex/Controllers/TeacherController.cs
@@ -9,21 +9,30 @@ using Codex.Models.SharedModels.SharedViewModels;
 
 namespace Codex.Controllers
 {
+
+    [Authorize(Roles = "Teacher")]
     public class TeacherController : Controller
     {
         private UserService _userService;
         private SubmissionService _submissionService;
         private AssignmentService _assignmentService;
+        private CourseService _courseService;
 
         public TeacherController() {
             _userService = new UserService();
             _submissionService = new SubmissionService();
             _assignmentService = new AssignmentService();
+            _courseService = new CourseService();
         }
 
         // GET: Teacher
         public ActionResult Index() {
+
+            var teacherId = _userService.GetUserIdByName(User.Identity.Name);
+
+
             return View();
+
         }
     }
 }
