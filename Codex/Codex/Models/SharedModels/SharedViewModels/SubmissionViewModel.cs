f345fda697b40cfaa75dc6d62a1ff55621046b0c
Hilmar Tryggvason <hilmart@gmail.com>
2016-05-10 11:11:00 +0000

    Added original filename to submission view model and also some student index stuff

--- Codex/Codex/Models/SharedModels/SharedViewModels/SubmissionViewModel.cs
+++ Codex/Codex/Models/SharedModels/SharedViewModels/SubmissionViewModel.cs
@@ -8,6 +8,7 @@ namespace Codex.Models.SharedModels.SharedViewModels
     public class SubmissionViewModel
     {
         public int Id { get; set; }
+        public string OriginalFilename { get; set; }
         public DateTime SubmissionTime { get; set; }
         public int? FailedTests { get; set; }
         public string Owner { get; set; }
