ad10282c6b055708dcd4c7161163ac9e47195bf2
birkirbrynjarsson <birkir@birkir.is>
2016-05-10 16:45:20 +0000

    Fixing Student ViewModel and Service Hierarchy

--- Codex/Codex/Models/StudentViewModels.cs
+++ Codex/Codex/Models/StudentViewModels.cs
@@ -3,9 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace Codex.Models
+namespace Codex.Models.StudentViewModels
 {
-    public class StudentViewModels
+    public class StudentViewModel
     {
+        public List<StudentAssignmentViewModel> Assignments { get; set; }
+    }
+    public class StudentAssignmentViewModel
+    {
+        public int Id { get; set; }
+        public string Course { get; set; }
+        public string Name { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public int MaxCollaborators { get; set; }
+        public double? AssignmentGrade { get; set; }
+        public bool IsDone { get; set; }
+        public List<CollaboratorViewModel> Collaborators { get; set; }
+        public List<StudentProblemViewModel> Problems { get; set; }
+    }
+    public class CollaboratorViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int GroupNumber { get; set; }
+    }
+    public class StudentProblemViewModel
+    {
+        public int Id { get; set; }
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Filetype { get; set; }
+        public string Attachment { get; set; }
+        public string Language { get; set; }
+        public List<StudentTestCaseViewModel> testCases { get; set; }
+        public int Weight { get; set; }
+        public int MaxSubmissions { get; set; }
+        public bool IsAccepted { get; set; }
+        public StudentSubmissionViewModel BestSubmission { get; set; }
+        public List<StudentSubmissionViewModel> Submissions { get; set; }
+    }
+    public class StudentSubmissionViewModel
+    {
+        public int Id { get; set; }
+        public string OriginalFilename { get; set; }
+        public DateTime SubmissionTime { get; set; }
+        public int? FailedTests { get; set; }
+        public string Owner { get; set; }
+    }
+    public class StudentTestCaseViewModel
+    {
+        public int Id { get; set; }
+        public int ProblemId { get; set; }
+        public string Input { get; set; }
+        public string ExpectedOutput { get; set; }
     }
 }
